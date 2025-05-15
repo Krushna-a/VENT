a1acf8f36e824533051d40d4e3e45d0f6229d37c
Krushna-a <Krushnaboinwad70@gmail.com>
2025-05-15 11:46:17 +0530

    cors problem solved

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const hackRouter = require("./Routes/hackathon");
 const MongoStore = require("connect-mongo")
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: 'https://vent-frontend.onrender.com',
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
