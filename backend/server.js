ff32d13e0d763f65dbb33bb1595a7cd77ddd8959
Krushna-a <krushnaboinwad70@gmail.com>
2025-05-21 23:58:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(
     }),
     cookie: {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     },
